13d612785efe404009dda29e0c1d7dc72a7a35e6
aleksei <Straikoid67@yandex.ru>
2022-07-10 21:50:49 +0300

    тест

---
+++ src/main/java/ru/praktikum/string_cheker/Account.java
@@ -0,0 +1,16 @@
+package ru.praktikum.string_cheker;
+
+public class Account {
+
+    private final String name;
+    private final String nameValidationRegex = "(^[A-ZА-ЯЁ][A-zА-яЁё'-]*+)( )([A-ZА-ЯЁ][A-zА-яЁё'-]*+$)";
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        return (name.length() <= 19
+                && name.matches(nameValidationRegexxx));
+    }
+}
