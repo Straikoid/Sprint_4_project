8465aeebe7627f1f4aaa89a6352f41e8f99c3a05
aleksei <Straikoid67@yandex.ru>
2022-07-11 19:50:38 +0300

    правка

--- src/main/java/ru/praktikum/string_cheker/Account.java
+++ src/main/java/ru/praktikum/string_cheker/Account.java
@@ -11,6 +11,6 @@ public class Account {
 
     public boolean checkNameToEmboss() {
         return (name.length() <= 19
-                && name.matches(nameValidationRegexxx));
+                && name.matches(nameValidationRegex));
     }
 }
